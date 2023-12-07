363087377626c304e3447a3d0c1e07d642dbaf3e
KUN1007 <yuyuyukunkunkun@gmail.com>
2023-12-07 11:39:19 +0800

    feat: giscus style

---
+++ .vitepress/components/giscus/Giscus.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import Giscus from '@giscus/vue'
+</script>
+
+<template>
+  <div class="comments-container">
+    <Giscus
+      id="comments"
+      repo="KUN1007/soft.moe"
+      repoId="R_kgDOKOgQEA"
+      category="General"
+      categoryId="DIC_kwDOKOgQEM4Cbjqa"
+      mapping="pathname"
+      term="Welcome to SOFT! MOE!"
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="top"
+      :theme="`../../theme/giscus/giscus.css`"
+      lang="en"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.comments-container {
+  max-height: 640px;
+  overflow-y: scroll;
+}
+</style>
